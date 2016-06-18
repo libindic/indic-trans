0d363d196eed03be00978c885f8e6b0f0a63af5f
Irshad Ahmad Bhat <bhatirshad127@gmail.com>
2016-06-18 22:45:26 +0530

    added tests for Malayalam-English model

--- indictrans/tests/test_trans.py
+++ indictrans/tests/test_trans.py
@@ -11,8 +11,8 @@ class TestTransliterator(TestCase):
 
     def setUp(self):
         super(TestTransliterator, self).setUp()
-        source = ['hin', 'ben']
-        target = ['eng', 'eng']
+        source = ['hin', 'ben', 'mal']
+        target = ['eng', 'eng', 'eng']
         self.src2trg = zip(source, target)
         self.trg2src = zip(target, source)
         self.test_dir = os.path.dirname(os.path.abspath(__file__))
