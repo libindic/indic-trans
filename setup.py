52c4b6e25f81c591b363e35089dc1f2089484557
Vasudev Kamath <vasudev@copyninja.info>
2016-06-25 15:41:09 +0530

    Drop autogenerated files of cython
    We can't include them in repo as they are machine dependent so this
    should be generated during build.

--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+
+extensions = [
+    Extension(
+        "indictrans._decode.beamsearch",
+        [
+            "indictrans/_decode/beamsearch.pyx"
+        ]
+    ),
+    Extension(
+        "indictrans._decode.viterbi",
+        [
+            "indictrans/_decode/viterbi.pyx"
+        ]
+    )
+
+]
 
 setup(
     setup_requires=['pbr'],
     pbr=True,
+    ext_modules=cythonize(extensions)
 )
