94ab13adb495f442f306870d13c285e57fe4a2a7
Irshad Ahmad Bhat <bhatirshad127@gmail.com>
2016-07-31 06:40:41 +0530

    added `numpydoc` extension for read-the-docs to read docstrings correctly

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.coverage',
+    'numpydoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
